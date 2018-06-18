6de7bb7fe842b954fcb0b8d713f6090da50ec12c
moshe770 <mshperling770@gmail.com>
2018-06-18 13:08:25 +0300

    ## [4.4.10] - 2018-04-08 15:53:00 ### Added - Custom product update event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [4.4.10] - 2018-04-08 15:53:00
+### Added
+- Custom product update event
+
 ## [4.4.8] - 2018-03-11 15:53:00
 ### Added
 - Category names index
---
+++ Observer/IspProductSave.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * IspProductSave File
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright 2014 Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace Autocompleteplus\Autosuggest\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * IspProductSave
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright 2014 Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
+class IspProductSave implements ObserverInterface
+{
+    /**
+     * Catalog helper
+     *
+     * @var \Autocompleteplus\Autosuggest\Helper\Batches
+     */
+    protected $helper;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    protected $date;
+
+    /**
+     * @var \Autocompleteplus\Autosuggest\Model\ResourceModel\Batch\Collection
+     */
+    protected $batchCollection;
+
+    /**
+     * ProductSave constructor.
+     * @param \Autocompleteplus\Autosuggest\Helper\Data $helper
+     * @param \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurable
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $date
+     * @param \Autocompleteplus\Autosuggest\Model\ResourceModel\Batch\CollectionFactory $batchCollectionFactory
+     * @param \Magento\Catalog\Model\Product $productModel
+     * @param \Autocompleteplus\Autosuggest\Model\Batch $batchModel
+     */
+    public function __construct(
+        \Autocompleteplus\Autosuggest\Helper\Batches $helper,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Stdlib\DateTime\DateTime $date
+    ) {
+        $this->helper = $helper;
+        $this->logger = $logger;
+        $this->date = $date;
+    }
+
+
+    /**
+     * Update products
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $product = $observer->getEvent()->getProduct();
+        $origData = $product->getOrigData();
+        $storeId = $product->getStoreId();
+        $productId = $product->getId();
+        $sku = $product->getSku();
+        $dt = $this->date->gmtTimestamp();
+        if (is_array($origData) &&
+            array_key_exists('sku', $origData)) {
+            $oldSku = $origData['sku'];
+            if ($sku != $oldSku) {
+                $this->helper->writeProductDeletion($oldSku, $productId, 0, $dt, $product);
+            }
+        }
+
+        //recording disabled item as deleted
+        if ($product->getStatus() == '2') {
+            $this->helper->writeProductDeletion($sku, $productId, 0, $dt, $product);
+            return $this;
+        }
+
+        $this->helper->writeProductUpdate($product, $productId, $storeId, $dt, $sku);
+
+        return $this;
+    }
+
+}
