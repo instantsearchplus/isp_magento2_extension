9782d31e92b3bc9e5ff9f121a752e3c326594f37
moshe770 <mshperling770@gmail.com>
2019-05-13 11:17:37 +0300

    ## [4.7.3] - 2019-05-13 11:47:00 ### Added - 2.3 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [4.7.3] - 2019-05-13 11:47:00
+### Added
+- 2.3 support
+
 ## [4.7.2] - 2019-04-14 11:47:00
 ### Fixed
 - Not indexed items support, phase 2
---
+++ Helper/Plugin/CsrfValidatorSkip.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * CatalogRule.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright 2018 Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace Autocompleteplus\Autosuggest\Helper\Plugin;
+
+use Magento\Framework\App\Helper;
+
+class CsrfValidatorSkip
+{
+	/**
+     * @param \Magento\Framework\App\Request\CsrfValidator $subject
+     * @param \Closure $proceed
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\App\ActionInterface $action
+     */
+    public function aroundValidate(
+        $subject,
+        \Closure $proceed,
+        $request,
+        $action
+    ) {
+        if ($request->getModuleName() == 'autocompleteplus') {
+            return; // Skip CSRF check
+        }
+        $proceed($request, $action); // Proceed Magento 2 core functionalities
+    }
+}
+
+?>
