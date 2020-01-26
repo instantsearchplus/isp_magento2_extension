0203fdf10edca7ac5c3d5c10866eb09d6319a951
moshe770 <mshperling770@gmail.com>
2020-01-26 09:12:59 +0200

    ## [4.7.25] - 2020-01-26 17:54:00 ### Added - Isp+ Sitemap support ### Fixed - Landing pages creation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [4.7.25] - 2020-01-26 17:54:00
+### Added
+- Isp+ Sitemap support
+### Fixed
+- Landing pages creation
+
 ## [4.7.24] - 2020-01-21 17:54:00
 ### Fixed
 - Compare price rendering
--- Controller/Products/Updatesitemap.php
+++ Controller/Products/Updatesitemap.php
@@ -1,23 +1,197 @@
 <?php
 
 namespace Autocompleteplus\Autosuggest\Controller\Products;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
+/**
+ * Class Updatesitemap
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
 class Updatesitemap extends \Autocompleteplus\Autosuggest\Controller\Products
 {
-    protected $productFactory;
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
+    protected $resultJsonFactory;
+
+    /**
+     * @var \Autocompleteplus\Autosuggest\Helper\Data
+     */
+    protected $helper;
+
+    /**
+     * @var \Autocompleteplus\Autosuggest\Helper\Api
+     */
+    protected $apiHelper;
+
+    protected $directory;
+
+    protected $_file;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Catalog\Model\ProductFactory $productFactory
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Autocompleteplus\Autosuggest\Helper\Data $helper,
+        \Autocompleteplus\Autosuggest\Helper\Api $apiHelper,
+        \Magento\Framework\Filesystem\DirectoryList $directoryList,
+        \Magento\Framework\Filesystem $filesystem
     ) {
-        $this->productFactory = $productFactory;
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->helper = $helper;
+        $this->apiHelper = $apiHelper;
+        $this->directory = $filesystem->getDirectoryWrite(DirectoryList::ROOT);
+        $this->_file = 'robots.txt';
+        $this->_fileFullPath = sprintf('%s%s%s', $directoryList->getRoot(), DIRECTORY_SEPARATOR, $this->_file);
         parent::__construct($context);
     }
 
     public function execute()
     {
-        //$collection = $this->categoryFactory->create()->getCollection();
-        //$collection->addFieldToSelect('*');
-        // TODO: Implement execute() method.
+        $response = array();
+        $result = $this->resultJsonFactory->create();
+
+        $storeId = $this->getRequest()->getParam('store_id', 1);
+        $uuid = $this->getRequest()->getParam('u', null);
+        $key = $this->getRequest()->getParam('authentication_key', null);
+        $state = $this->getRequest()->getParam('state', 'on');
+
+        $siteUrl = $this->helper->getStoreUrl();
+
+        if ($this->isValid($uuid, $key)) {
+            $sitemapUrl = 'Sitemap:http://magento.instantsearchplus.com/ext_sitemap?u=' . $uuid . '&store_id=' . $storeId;
+            if ($state == 'on') {
+                $response = $this->insertSiteMap($sitemapUrl);
+            } else {
+                $response = $this->removeSiteMap($sitemapUrl);
+            }
+
+        } else {
+            $response = [
+                'success' => false,
+                'error' => 'Authentication failed'
+            ];
+        }
+        $result->setData($response);
+        return $result;
+    }
+
+    public function isValid($uuid, $authKey)
+    {
+        if ($this->apiHelper->getApiUUID() == $uuid
+            && $this->apiHelper->getApiAuthenticationKey() == $authKey
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $sitemapUrl
+     * @return array
+     * @throws \Magento\Framework\Exception\FileSystemException
+     */
+    protected function removeSiteMap($sitemapUrl)
+    {
+        $write = false;
+        if ($this->directory->isFile($this->_file)) {
+            if (strpos($this->directory->readFile($this->_file), $sitemapUrl) == true) {
+                $write = true;
+            } else {
+                $response = [
+                    'success' => true
+                ];
+            }
+        } else {
+            $response = [
+                'success' => true
+            ];
+        }
+        //UPDATE EXISTING ROBOTS.TXT
+        if ($write) {
+            if ($this->directory->isWritable($this->_file)) {
+
+                $oldContent = $this->directory->readFile($this->_file);
+                $newContent = str_replace($sitemapUrl, '', $oldContent);
+                $this->directory->writeFile($this->_file, $newContent, 'w');
+                $response = [
+                    'success' => true
+                ];
+            } else {
+                //write message that file is not writteble
+                $response = [
+                    'success' => false,
+                    'error' => 'File ' . $this->_fileFullPath . ' is not writable.'
+                ];
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * @param $sitemapUrl
+     * @return array
+     * @throws \Magento\Framework\Exception\FileSystemException
+     */
+    protected function insertSiteMap($sitemapUrl)
+    {
+        $write = false;
+        if ($this->directory->isFile($this->_file)) {
+            if (strpos($this->directory->readFile($this->_file), $sitemapUrl) === false) {
+                $write = true;
+            } else {
+                $response = [
+                    'success' => true
+                ];
+            }
+        } else {
+            if ($this->directory->isWritable()) {
+                //create robots sitemap
+                $this->directory->writeFile($this->_file, $sitemapUrl);
+                $response = [
+                    'success' => true
+                ];
+            } else {
+                $response = [
+                    'success' => false,
+                    'error' => 'File ' . $this->_fileFullPath . ' is not writable.'
+                ];
+            }
+        }
+        //UPDATE EXISTING ROBOTS.TXT
+        if ($write) {
+            if ($this->directory->isWritable($this->_file)) {
+                /**
+                 * Append sitemap
+                 */
+                $sitemapUrl = "\n\r" . $sitemapUrl;
+                $this->directory->writeFile($this->_file, $sitemapUrl, 'a');
+                $response = [
+                    'success' => true
+                ];
+            } else {
+                //write message that file is not writteble
+                $response = [
+                    'success' => false,
+                    'error' => 'File ' . $this->_fileFullPath . ' is not writable.'
+                ];
+            }
+        }
+        return $response;
     }
 }
